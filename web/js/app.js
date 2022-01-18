43048c44966aba8b81b0bbb0bc23bad9b68e094f
lhnaseeb63 <lhnaseeb@gmail.com>
2022-01-18 11:32:58 +0300

    split into day 1 and day 1 ALT. App declaration now in separate file

--- web/js/app.js
+++ web/js/app.js
@@ -1,21 +1,22 @@
-var app = angular.module("adventOfCode",["ngRoute"]);
-
-
-  app.config(function ($routeProvider) {
-    $routeProvider
-    .when('/', {
-        templateUrl:'../index.html'
+var app = angular
+.module("adventOfCode", ["ngRoute"])
+.config(function ($routeProvider, $locationProvider) {
+  //inject $locationProvider service
+  $locationProvider.hashPrefix(""); // add configuration
+  $routeProvider
+    .when("/home", {
+      template: "About Us",
     })
-    .when('/day1', {
-     //   controller:'day1Controller',
-        templateUrl:'../views/day1.html'
+    .when("/day1", {
+      templateUrl: "views/day1.html",
+      controller: "day1Controller",
     })
-    .when('/day2', {
-    //    controller:'day2Controller',
-        templateUrl:'../views/day2.html'
-    })
-    .otherwise({
-        redirectTo:'/'
-    })
+    .when("/day1ALT", {
+        templateUrl: "views/day1ALT.html",
+        controller: "day1ALTController",
+      })
+    .when("/day2", {
+      templateUrl: "views/day2.html",
+      controller: "day2Controller",
+    });
 });
- 
